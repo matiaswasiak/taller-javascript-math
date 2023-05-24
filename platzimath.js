3df5c4c3869113fa440fb0c5f9575b3be2804d05
matiaswasiak <matiaswasiak@gmail.com>
2023-05-24 09:02:32 -0300

    Método reduce

--- platzimath.js
+++ platzimath.js
@@ -1,11 +1,24 @@
 function calcularPromedio(array) {
   // sumar todos los elementos del array / cantidad de elementos
-  let sumaArray = 0;
+  // let sumaArray = 0;
+  // for (let i = 0; i < array.length; i++) {
+  // sumaArray += array[i];
+  // }
 
-  for (let i = 0; i < array.length; i++) {
-    sumaArray += array[i];
-  }
+  const sumaLista = array.reduce(function (valorAcumulado = 0, nuevoElemento) {
+    return valorAcumulado + nuevoElemento;
+  });
 
-  const promedio = sumaArray / array.length;
+  // const sumaListArrow = array.reduce(
+  //   (valorAcumulado = 0, nuevoElemento) => valorAcumulado + nuevoElemento
+  // );
+
+  const promedio = sumaLista / array.length;
   console.log(promedio);
 }
+
+function arrayIsEven(array) {
+  return array.length % 2 === 0;
+}
+
+const arrayIsEvenArrow = (array) => array.length % 2 === 0;
