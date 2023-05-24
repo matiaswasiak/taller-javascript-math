3e0af2f8e5323d02b6f6df1203fb99ad2fac10bc
matiaswasiak <matiaswasiak@gmail.com>
2023-05-24 07:09:31 -0300

    Cupones de descuento

--- descuentos.js
+++ descuentos.js
@@ -1,15 +1,66 @@
 const inputPrice = document.querySelector("#price");
-const inputDiscount = document.querySelector("#discount");
-const btn = document.querySelector("#calculate");
-const resultP = document.querySelector("#result");
+const inputCoupon = document.querySelector("#coupon");
+const btnPrice = document.querySelector("#calculate");
+const result = document.querySelector("#result");
 
-btn.addEventListener("click", calculatePriceWithDiscount);
+btnPrice.addEventListener("click", calculatePriceWithDiscount);
 
 function calculatePriceWithDiscount() {
-  const price = inputPrice.value;
-  const discount = inputDiscount.value;
+  const price = Number(inputPrice.value);
+  const coupon = inputCoupon.value;
+
+  const coupons = {
+    hopefully_nobody_uses_this_coupon: 100,
+    who_says_no_to_a_coupon: 50,
+    yes_this_is_a_coupon: 10,
+  };
+
+  if (!price || !coupon) {
+    result.innerText = "Debes ingresar un precio y un cupón";
+    return;
+  }
+
+  let discount;
+
+  // USING IF
+  // if (coupon === "soy_un_cupon_de_100%") {
+  //   discount = 100;
+  // } else if (coupon === "who_says_no_to_a_coupon?") {
+  //   discount = 50;
+  // } else if (coupon === "is_this_a_coupon?") {
+  //   discount = 25;
+  // } else if (coupon === "yes_this_is_a_coupon") {
+  //   discount = 10;
+  // } else {
+  //   result.innerText = `El cupón ${coupon} no es válido`;
+  // }
+
+  // USING SWITCH
+  // switch (coupon) {
+  //   case "soy_un_cupon_de_100%":
+  //     discount = 100;
+  //     break;
+  //   case "who_says_no_to_a_coupon?":
+  //     discount = 50;
+  //     break;
+  //   case "is_this_a_coupon?":
+  //     discount = 25;
+  //     break;
+  //   case "yes_this_is_a_coupon":
+  //     discount = 10;
+  //     break;
+  //   default:
+  //     result.innerText = `El cupón ${coupon} no es válido`;
+  //     return;
+  // }
+
+  if (coupons[coupon]) {
+    discount = coupons[coupon];
+  } else {
+    result.innerText = `El cupón ${coupon} no es válido`;
+  }
 
   const newPrice = (price * (100 - discount)) / 100;
 
-  resultP.innerText = `El precio con descuento es: $${newPrice}`;
+  result.innerText = `El precio con descuento es de $${newPrice}`;
 }
